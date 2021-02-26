bfa4f8842618af11c90df8ce488fdb4bfed37de0
Brad Clawsie <brad@b7j0c.org>
2021-02-26 13:37:38 -0600

    added

---
+++ raku-pkgs.sh
@@ -0,0 +1,12 @@
+zef install Linenoise App::Prove6
+zef install LibUUID
+zef install --/test cro
+zef install Base64::Native
+zef install Gcrypt
+zef install DBIish
+zef install File::Directory::Tree
+zef install OO::Monitors
+zef install Log::Async
+zef install LibCurl
+zef install JSON::Fast
+zef install Cro::HTTP::Test
