f6acfadb497806c26aeee59d6719d282fdbb5247
Brad Clawsie <brad@b7j0c.org>
2021-04-25 17:40:26 -0500

    rm

--- guile-pkgs.sh
+++
@@ -1,7 +0,0 @@
-wget https://download.savannah.gnu.org/releases/guile-json/guile-json-4.5.2.tar.gz
-tar -xzf guile-json-4.5.2.tar.gz
-cd guile-json-4.5.2
-./configure --prefix=$HOME/guile --libdir=$HOME/guile/lib
-make install
-cd ..
-rm -rf guile-json*
