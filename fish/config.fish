83aaf5fe4e9ae51607bb4e0fb7c8bbbbaaa10662
Brad Clawsie <brad@b7j0c.org>
2023-02-20 18:53:18 -0600

    editor = helix

--- fish/config.fish
+++ fish/config.fish
@@ -22,9 +22,9 @@ if status is-interactive
     set -gx SAVEHIST 10000
 
     # default editor
-    set -gx EDITOR emacs
+    set -gx EDITOR helix
     set -gx ALTERNATE_EDITOR helix
-    set -gx VISUAL emacs
+    set -gx VISUAL helix
 
     # gnupg
     set -gx GPG_TTY (tty)
