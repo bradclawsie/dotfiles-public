11cb6bd0d23893124125c601cfaf4e02b17adf9f
Brad Clawsie <brad@b7j0c.org>
2023-09-26 18:16:06 -0500

    back to emacs

--- fish/config.fish
+++ fish/config.fish
@@ -19,9 +19,9 @@ if status is-interactive
     set -gx SAVEHIST 10000
 
     # default editor
-    set -gx EDITOR helix
+    set -gx EDITOR emacsclient
     set -gx ALTERNATE_EDITOR nano
-    set -gx VISUAL helix
+    set -gx VISUAL emacsclient
 
     # gnupg
     set -gx GPG_TTY (tty)
