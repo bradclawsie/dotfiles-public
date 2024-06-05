a039a047a7ea3db7f10890d6d97ab75cccd788c0
Brad Clawsie <brad@b7j0c.org>
2024-06-05 12:55:35 -0500

    rm zig

--- fish/config.fish
+++ fish/config.fish
@@ -3,25 +3,20 @@ alias h="history"
 alias gpg-agent-restart="gpg-connect-agent reloadagent /bye"
 
 if status is-interactive
-    # locale
     set -gx LANG en_US.UTF-8
     set -gx LC_ALL en_US.UTF-8
 
-    # history
     set -gx HISTFILE $HOME/.histfile
     set -gx HISTSIZE 10000
     set -gx SAVEHIST 10000
 
-    # default editor
     set -gx EDITOR helix
     set -gx ALTERNATE_EDITOR nano
     set -gx VISUAL helix
 
-    # gnupg
     set -gx GPG_TTY (tty)
     gpg-connect-agent updatestartuptty /bye >/dev/null
 
-    # ssh-agent
     set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 
     clear
@@ -33,9 +28,7 @@ set -gx PERL_READLINE_NOWARN yes
 
 fish_add_path -p $HOME/bin
 fish_add_path -p $HOME/go/bin
-fish_add_path -p $HOME/.zig
 
-# optional env vars
 if test -f ~/.config/fish/env.fish
     source ~/.config/fish/env.fish
 end
