c108c4017c10d0444980684c6289e77b432198cf
Brad Clawsie <brad@b7j0c.org>
2024-07-13 15:57:41 -0500

    env

--- fish/env.fish
+++ fish/env.fish
@@ -1,3 +1,4 @@
 set -gx REPOSITORY_BASE /tmp
 set -gx POSTGRES_APP_URL postgres://grokloc:grokloc@localhost:5432/app
-
+set -gx CGO_ENABLED 1
+set -gx PERL5LIB $HOME/repos/bradclawsie/grokloc-perl5/lib
