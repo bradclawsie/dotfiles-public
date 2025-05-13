c4b151c2b52311df731814cb72d1ba22ed90856e
Brad Clawsie <brad@b7j0c.org>
2025-05-13 00:46:28 -0500

    chromeos env

--- fish/chromeos-env.fish
+++ fish/chromeos-env.fish
@@ -1,4 +1,5 @@
 set -gx POSTGRES_APP_URL postgres://grokloc:grokloc@localhost:5432/app
+set -gx DATABASE_URL postgres://grokloc_root:grokloc@localhost:5432/app
 set -gx REPOSITORY_BASE /tmp
 set -gx CGO_ENABLED 1
 set -gx HELIX_RUNTIME "$HOME/repos/helix/runtime"
