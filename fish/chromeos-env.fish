0f8635c4f1be3ac197edcc087aec33e0c7d6dee8
Brad Clawsie <brad@b7j0c.org>
2025-07-16 13:37:38 -0500

    move to deb helix

--- fish/chromeos-env.fish
+++ fish/chromeos-env.fish
@@ -2,7 +2,6 @@ set -gx POSTGRES_APP_URL postgres://grokloc:grokloc@localhost:5432/app
 set -gx DATABASE_URL postgres://grokloc_root:grokloc@localhost:5432/app
 set -gx REPOSITORY_BASE /tmp
 set -gx CGO_ENABLED 1
-set -gx HELIX_RUNTIME "$HOME/repos/helix/runtime"
 
 . ~/perl5/perlbrew/etc/perlbrew.fish
 /home/brad/.rakubrew/bin/rakubrew init Fish | source
