6e07cd5e263025811b4d997c519620540a989342
Brad Clawsie <brad@b7j0c.org>
2025-08-09 14:35:11 -0500

    rm env vars

--- fish/env.fish
+++ fish/env.fish
@@ -1,6 +1,3 @@
-set -gx POSTGRES_APP_URL postgres://grokloc:grokloc@localhost:5432/app
-set -gx DATABASE_URL postgres://grokloc_root:grokloc@localhost:5432/app
-set -gx REPOSITORY_BASE /tmp
 set -gx CGO_ENABLED 1
 
 $HOME/.rakubrew/bin/rakubrew init Fish | source
