659a7c658728da3909abc165cd4852d92efa695a
Brad Clawsie <brad@b7j0c.org>
2025-11-08 09:50:42 -0600

    bring back telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,13 +3,60 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
--- Normal mode mapping
 vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
-
--- Insert mode mapping (saves and returns to insert mode)
 vim.keymap.set('i', '<C-s>', '<C-o>:w<CR>', { noremap = true, silent = true })
-
--- Visual mode mapping (saves and preserves selection)
 vim.keymap.set('v', '<C-s>', '<Esc>:w<CR>gv', { noremap = true, silent = true })
 
 vim.g.perl_host_prog = "/usr/bin/perl"
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+    },
+    config = function()
+      local telescope = require("telescope")
+
+      telescope.setup({
+        defaults = {
+          extensions = {
+            fzf = {
+              fuzzy = true,
+              override_generic_sorter = true,
+              override_file_sorter = true,
+              case_mode = "smart_case",
+            },
+          },
+        },
+      })
+
+      telescope.load_extension("fzf")
+
+      vim.keymap.set("n", "<leader>ff", '<cmd>Telescope find_files<cr>', { desc = "Find Files" })
+      vim.keymap.set("n", "<leader>fg", '<cmd>Telescope live_grep<cr>', { desc = "Live Grep" })
+      vim.keymap.set("n", "<leader>fb", '<cmd>Telescope buffers<cr>', { desc = "Find Buffers" })
+    end,
+  },
+})
