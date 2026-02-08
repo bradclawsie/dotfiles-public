b64311deacaeb7410d0d7363a42c18ffb0edbbe0
Brad Clawsie <brad@b7j0c.org>
2026-02-08 16:28:08 -0600

    env

--- fish/chromeos-env.fish
+++ fish/chromeos-env.fish
@@ -1,3 +1,11 @@
 set -gx CGO_ENABLED 1
 
+if test -d $HOME/local/go/bin
+    fish_add_path -p $HOME/local/go/bin
+end
+
+if test -d $HOME/local/dmd2/linux/bin64
+    fish_add_path -p $HOME/local/dmd2/linux/bin64
+end
+
 . ~/perl5/perlbrew/etc/perlbrew.fish
